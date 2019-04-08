5ce14e2ac47714edc157e7e2a4aadaf161abb6ab
Ankur Goswami <ankur.goswami12@gmail.com>
2019-04-08 15:22:04 -0500

    Fill out an embeddings db, WSJ retrieval

--- model/run.py
+++ model/run.py
@@ -2,7 +2,42 @@ import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, PickleType, String
+from sqlalchemy.orm import sessionmaker
 
+
+base = declarative_base()
+engine = create_engine('sqlite:///:memory:', echo=True)
+Session = sessionmaker(bind=engine)
+
+class Embeddings(base):
+    __tablename__ = 'embeddings'
+    word = Column(String, primary_key=True)
+    embedding = Column(PickleType)
+
+    def __repr__(self):
+        return f'<Embedding(word={self.word}, embedding={self.embedding})>'
+
+
+class EmbeddingsDB():
+
+    def __init__(self, session):
+        self.session = session
+
+
+    def load_glove(self, embeddings_file):
+        with open(embeddings_file, 'r') as ef:
+            for line in eff:
+                line = line[:-1] # shave the new line
+                spl = line.split(' ')
+                word = spl[0]
+                embedding = spl[1:]
+                embedding = [float(x) for x in embedding]
+                new_word = Embeddings(word=word, embedding=embedding)
+                self.session.add(new_word)
+            self.session.commit()
 
 class RandomSequenceDataset(Dataset):
 
@@ -23,6 +58,49 @@ class RandomSequenceDataset(Dataset):
         return sample
 
 
+class WSJ(Dataset):
+
+    def read_wsj(self, file_path):
+        with open(file_path, 'r') as rf:
+            current_example = None
+            for line in rf:
+                if line == '\n':
+                    new_sequence, new_sequence_labels = zip(*current_example)
+                    self.sequences.append(new_sequence)
+                    self.sequence_labels(new_sequence_labels)
+                    current_example = None
+                    continue
+                line = line[:-1] # shave the newline
+                spl = line.split(' ')
+                word, _, label = spl
+                if label not in self.labels:
+                    self.labels.append(label)
+                label_ind = self.labels
+                if current_example is None:
+                    current_example = [(word, label_ind)]
+                else:
+                    current_example.append((word, label_ind))
+
+    def __init__(self, file_path):
+        edb = EmbeddingsDB(session)
+        edb.load_glove('data/embeddings/glove.6B.50d.txt')
+        self.sequences = []
+        self.sequence_labels = []
+        self.labels = []
+        self.read_wsj(file_path)
+        print(self.sequences)
+        print(self.sequence_labels)
+
+
+    def __len__(self):
+        return len(self.sequences)
+
+    def __getitem__(self, idx):
+        sample = {'sequence': self.sequences[idx], 'labels': self.sequence_labels[idx]}
+        return sample
+
+
+
 class SequenceClassifier(torch.nn.Module):
     def __init__(self, sequence_len, hidden_size):
         super(SequenceClassifier, self).__init__()
@@ -37,7 +115,7 @@ class SequenceClassifier(torch.nn.Module):
         return preds
 
 if __name__ == '__main__':
-    sequence_dataset = RandomSequenceDataset(10000)
+    sequence_dataset = WSJ('data/wsj/train.txt')
     loader = DataLoader(sequence_dataset)
     sequence_len = 10
     hidden_size = 20
